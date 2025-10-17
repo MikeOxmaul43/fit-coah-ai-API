ece55c4a9797aaad220cea98c140af1969e4fe4b
MikeOxmaul <leva35az@gmail.com>
2025-10-18 00:56:09 +0300

    Create register method in service

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,1 +1,38 @@
 package auth
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"sportTrackerAPI/internal/user"
+)
+
+type Service struct {
+	*user.Repository
+}
+
+func NewAuthService(repository *user.Repository) *Service {
+	return &Service{repository}
+}
+
+func (service *Service) Register(email, password, userName string) (string, error) {
+	existedUser, _ := service.Repository.FindByEmail(email)
+	if existedUser != nil {
+		return "", errors.New("ErrUserExisted")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	user := user.User{
+		UserName:       userName,
+		Email:          email,
+		HashedPassword: string(hashedPassword)}
+
+	_, err = service.Repository.Create(&user)
+	if err != nil {
+		return "", err
+	}
+	return user.Email, nil
+}
