847952c22b62600b9e3d474014b93726c1b44f8c
MikeOxmaul <leva35az@gmail.com>
2025-10-31 23:36:11 +0300

    create methods in exercise repo

--- internal/exercise/repository.go
+++ internal/exercise/repository.go
@@ -10,11 +10,36 @@ type Repository struct {
 
 func NewExerciseRepository(dataBase *pkg.Db) *Repository { return &Repository{DataBase: dataBase} }
 
-func (repo *Repository) FindByName(name string) (*Exercise, error) {
+func (repo *Repository) GetAll() ([]Exercise, error) {
+	var exercises []Exercise
+	err := repo.DataBase.DB.Find(&exercises).Error
+	if err != nil {
+		return nil, err
+	}
+	return exercises, nil
+}
+
+func (repo Repository) Update(exercise Exercise) error {
+	return repo.DataBase.DB.Save(exercise).Error
+}
+
+func (repo Repository) Delete(id uint) error {
+	return repo.DataBase.DB.Delete(&Exercise{}, id).Error
+}
+func (repo *Repository) GetByName(name string) (*Exercise, error) {
 	var exercise Exercise
 	result := repo.DataBase.First(&exercise, "name = ?", name)
 	if result.Error != nil {
 		return nil, result.Error
 	}
 	return &exercise, nil
+}
+
+func (repo *Repository) GetAllByMuscleGroup(muscleGroup string) ([]Exercise, error) {
+	var exercises []Exercise
+	result := repo.DataBase.Find(&exercises, "muscle_group = ?", muscleGroup)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return exercises, nil
 }
